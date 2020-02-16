d50d0dbff5e0dd5692d5dff3f60f213110bbc38f
abdulhakimdarman <abdul10hakim@gmail.com>
2020-02-16 00:15:18 -0500

    Insertion Sort Complete.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,15 +2,33 @@
 public class InsertionSort {
 	public static void main(String[] args) {
 		/*
-		 * In place algorithm
+		 * In place algorithm ('ω^＼)
 		 * 
-		 * 0(n^2) time complexxity - quadratic
+		 * 0(n^2) time complexity - quadratic (┛◉Д◉)┛彡┻━┻ 
 		 * 
-		 * it will take 100 steps to sort 10 items
+		 * it will take 100 steps to sort 10 items ôヮô 
 		 * 
-		 * stable algorithm
+		 * Stable algorithm - everything is literally in order ＼(￣▽￣)/
 		 */
 		
+		int[] intArray = {20, 35, -15, 7, 55, 1 -22};
+		
+		for(int firstUnsortedIndex = 1;firstUnsortedIndex < intArray.length;
+		firstUnsortedIndex++) {
+			int newElement = intArray[firstUnsortedIndex];
+			// We will use this i outside the following loop
+			int i;
+			
+			for(i = firstUnsortedIndex;i > 0 && intArray[i - 1] > newElement;i--) {
+				intArray[i] = intArray[i-1];
+			}
+			
+			intArray[i] = newElement;
+		}
+		
+		for(int a:intArray) {
+			System.out.print(a + " ");
+		}
 		
 		
 	}
