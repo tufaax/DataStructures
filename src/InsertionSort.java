ce2a94db4ad6f49f58b15020b3139853e1605cee
abdulhakimdarman <abdul10hakim@gmail.com>
2020-02-15 23:50:38 -0500

    Leggo :P

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,4 +1,18 @@
 
 public class InsertionSort {
+	public static void main(String[] args) {
+		/*
+		 * In place algorithm
+		 * 
+		 * 0(n^2) time complexxity - quadratic
+		 * 
+		 * it will take 100 steps to sort 10 items
+		 * 
+		 * stable algorithm
+		 */
+		
+		
+		
+	}
 
 }
